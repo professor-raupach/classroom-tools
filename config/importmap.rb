f479ce358dbfca8f7330dc23d60517e5db0d6aef
Paul Raupach <paul@raupach.com>
2025-05-31 15:35:09 -0700

    bootstrap is working

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap" # @5.3.6
+# pin "bootstrap" # @5.3.6
+# pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
